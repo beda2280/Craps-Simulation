5142f5fbea27c5db385af923a9d15fe00c02534c
beda2280 <beda2280@OSS326-06.stthomas.edu>
2020-02-28 20:11:34 -0600

    test test test

--- src/PlayWithInheritance.java
+++ src/PlayWithInheritance.java
@@ -19,7 +19,7 @@ public class PlayWithInheritance
 	public static int RUN_EXAMPLE = 1; // change below to run different methods
 										// thus exploring different behaviors
 
-	public static void main(String[] args)
+	public static void Main(String[] args)
 	{
 		StdOut.println();
 
